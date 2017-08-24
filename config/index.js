7c500b6274134acc6fef23e33e1208d1103f408f
mohong <mohong@xinlebao.com.cn>
2017-08-24 11:17:48 +0800

    初始化系统

---
+++ config/index.js
@@ -0,0 +1,12 @@
+const production = {
+	port: process.env.PORT || 13606,
+	mongo_host: 'mongodb://39.108.137.234:28332/wchat',
+	allow_origin: 'http://mohng.com'
+};
+
+const development = Object.assign({}, production, {
+	port: 30600,
+	allow_origin: 'http://localhost:8808'
+});
+
+module.exports = process.env.NODE_ENV === 'production' ? production : development;
